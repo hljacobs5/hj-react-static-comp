4a62ba75df3d0bd6df3131e1a0d32bf193119cbb
Haley Jacobs <haleyljacobs@gmail.com>
2018-05-21 21:43:01 -0600

    Add stories

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 const stories = [
   {
-    img: './story1.jpg',
+    img: '/public/story1.jpg',
     title: 'Winning at Fonts',
     desc: 'or, Why you should care about typography',
     authImg: './louisa.png',
@@ -15,37 +15,41 @@ const stories = [
     estTime: '2 min'
   },
   {
-    img: './story2.jpg',
+    img: '/public/story2.jpg',
     title: 'Hoodlums: a dissertation',
     desc: 'Examining villains in modern storytelling',
-    authImg: './alternate.png',
+    authImg: '/public/alternate.png',
     authName: 'Nathaniel',
     estTime: '10 min'
   },
   {
-    img: './story3.jpg',
+    img: '/public/story3.jpg',
     title: 'Halloween is a year-round sport',
     desc: 'Those who say otherwise just aren\'t trying hard enough.',
-    authImg: './leta.png',
+    authImg: '/public/leta.png',
     authName: 'Leta',
     estTime: '8 min'
   },
   {
-    img: './story4.jpg',
+    img: '/public/story4.jpg',
     title: 'How to Improve Learning and Classroom Engagement with Humor',
     desc: 'Okay, so first of all, everybody knows fart jokes are hilarious ...',
-    authImg: './pamela.png',
+    authImg: '/public/pamela.png',
     authName: 'Pamela',
     estTime: '11 min'
   }
 ]
 
-const App = () => {
+const App = (props) => {
   return (
     <div className='App'>
+    <Header headerTitle = 'Material'/>
       {/* add a Header component (already created) */}
       {/* add a Banner component (already created) */}
-      <Stories  storiesTitle="Today's Top Stories"
+    <Banner bannerTitle/>
+    <Stories  storiesTitle="Today's Top Stories"
+                stories={stories} />
+    <Stories  storiesTitle="Pop culture"
                 stories={stories} />
       {/* add another Stories component with storiesTitle 'Pop Culture', and just pass down the same stories array as the above Stories component (already created but will need some editing) */}
       {/* add a Footer Component (you will need to create this component in the provided Footer.js file) */}
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className='Header'>
-      {/* What goes in here? */}
+      <a href='sldjf'>Write a story</a>
+      <h3>{props.headerTitle}</h3>
+      <a href = 'sdf'>Sign in/Sign up</a>
     </div>
   )
 }
